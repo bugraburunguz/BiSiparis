05812e331e048baaa373baa0def223392e56a238
Gökhan ÖZTÜRK <GokhanOzturk@AndroidEdu.IO>
2018-02-09 22:34:10 +0300

    First Commit with Love <3

---
+++ app/src/main/java/io/android/bisiparis/MainActivity.kt
@@ -0,0 +1,41 @@
+package io.android.bisiparis
+
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    /**
+     * Login Ekranı
+     */
+// TODO (1) Personel Adı ve Personel Şifresi alanlarını koyunuz.
+// TODO (2) Personel Adını hatırlamak için checkbox koyunuz ve ticklenirse, Personel Adı alanına ne yazılıyorsa, uygulamadan çıkıp girdiğimizde personel adının otomatik yazılmasını sağlayınız.
+// TODO (3) Şifre alanını, şifre değiştiğinde *** olacak şekilde ayarlayınız.
+// TODO (4) Tüm text alanlarına "FFF9AA" renk kodunu koyunuz.
+// TODO (5) Giriş Yap Butonunun background'unu transparent yapınız.
+// TODO (6) Giriş Yap Butonunun Text Büyüklüğünü 18 yapın.
+// TODO (7) Giriş Yap Butonuna basıldığında Kullanıcı Adını Singleton Object'e koyunuz ve Dashboard Activity'den erişiniz. Ve Loglayınız.
+// TODO (8) Giriş Yap Butonunun text'ini bold yapınız.
+// TODO (9) Giriş Yap Butonuna basıldığında Dashboard Activity'e yönlen.
+// TODO (10) Butonu ilk başta pasif konuma alın. Tıklanabilir olmasın. Tüm editlenebilir alanlar dolduğunda tıklanabilir olmasını sağlayınız.
+
+    /**
+     * Dashboard Ekranı
+     */
+// TODO (1) Bu personel daha önce herhangi bir müşteri kayıt ettiyse bunları Recyclerview'da görebilmelidir.
+// TODO (2) Herhangi bir müşteri eklenmediyse -bold şekilde-, ekranda "Herhangi bir müşteri kaydı bulunamamaktadır." yazmalıdır.
+// TODO (3) Actionbar'a + iconu tanımlayın.
+// TODO (4) + iconunu svg dosyasından alınız.
+// TODO (5) + iconuna tıklandığında yemek sipariş ekranına gitmelisiniz.
+// TODO (6) Yeni bir müşteri eklendiyse, sağa ve sola RecyclerView item'i çekilerek silinebilmelidir.
+// TODO (7) SwpieRefreshLayout ekleyiniz ve refresh yapıldığında "Harikayım ben!" yazdırın.
+
+    /**
+     * Yemek Sipariş Ekranı
+     */
+// TODO (1) En başta şipariş türü yazmalıdır. Türler : Çorba, Ana Yemek, Tatlı, İçeçek.
+// TODO (2) Imajlar Grid RecyclerView olarak ekranda listelenmelidir.
+// TODO (3) Imajlardan herhangi bir seçim yapıldığında bir sonraki sipariş türü açılmaldıır. Bknz. Çorba seçiminden sonra  Ana Yemek, sonra Tatlı, sonra İçecek.
+// TODO (4) Kullanıcı bir seçim yaptığında yeni imajlar eklenmelidir.
+// TODO (5) Yapılan her seçim database'e eklenmelidir.
+// TODO (6) Tüm seçim yapıldığında DashboardActivity'e dönmelidir. Ve Eklenen müşteri listelenmelidir.
+}
